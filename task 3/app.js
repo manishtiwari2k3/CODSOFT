29085b33400dc00e4b02368a21447031918b3d43
Manish Tiwari <155799133+manishtiwari2k3@users.noreply.github.com>
2024-03-09 08:37:32 +0530

    Add files via upload

---
+++ task 3/app.js
@@ -0,0 +1,24 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+    } catch (error) {
+        displayValue = 'Error';
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('display').innerText = displayValue;
+}
